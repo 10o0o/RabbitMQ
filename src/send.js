9c0c722643d655e72a4bf64f5cf4b5d039198cba
10o0o <wotjs2923@gmail.com>
2021-07-14 18:01:57 +0900

    Promise apply

--- src/send.js
+++ src/send.js
@@ -3,23 +3,21 @@ const amqp = require('amqplib/callback_api');
 amqp.connect('amqp://localhost', (err0, connection) => {
   if (err0) throw err0;
 
-  connection.createChannel((err1, channel) => {
+  connection.createChannel(async (err1, channel) => {
     if (err1) throw err1;
 
     const queue = 'hello';
     const msg = 'Hello world';
 
-    channel.assertQueue(queue, {
+    return await new Promise(() => {
+      channel.assertQueue(queue, {
       durable: false,
-    });
-
-    channel.sendToQueue(queue, Buffer.from(msg));
-    console.log(`[x] Sent ${msg}`);
+      });
+      channel.sendToQueue(queue, Buffer.from(msg));
+      console.log(`[x] Sent ${msg}`);
+      console.log('여기')
+      connection.close();
+      process.exit(0);
+    }).catch(e => console.error(e));
   });
-
-  // 왜 async await 아무 효과 없는데 setTimeout 썼지?
-  setTimeout(() => {
-    connection.close();
-    process.exit(0);
-  }, 500);
 })
